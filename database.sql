e37a4732e643af6c3544d1db0abf05d2ab967da7
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-01-30 18:10:04 -0300

    create table funcionarios

--- database.sql
+++ database.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE curso_sql;
+
+USE curso_sql;
+
+CREATE TABLE funcionarios 
+(
+	id int unsigned not null auto_increment,
+    nome varchar(45) not null,
+    salario double not null default '0',
+    departamento varchar(45) not null,
+    PRIMARY KEY (id)
+);
