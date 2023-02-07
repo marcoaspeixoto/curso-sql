2e7c27855ab11bd5e29c6d3b76c4a715f911a6bd
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-02-07 15:58:18 -0300

    create table tipo

--- exercicio3_1.sql
+++ exercicio3_1.sql
@@ -2,3 +2,9 @@ CREATE DATABASE exercicio3_1;
 
 USE exercicio3_1;
 
+CREATE TABLE tipo
+(
+	codigo int unsigned not null auto_increment,
+    tipo varchar(40) not null,
+    PRIMARY KEY (codigo)
+);
