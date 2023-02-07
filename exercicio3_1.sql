5b8c2d660859105cefac5634b32548b3514eb5cc
Marco Peixoto <marcoaspeixoto@gmail.com>
2023-02-07 16:30:21 -0300

    create table cursos

--- exercicio3_1.sql
+++ exercicio3_1.sql
@@ -2,14 +2,14 @@ CREATE DATABASE exercicio3_1;
 
 USE exercicio3_1;
 
-CREATE TABLE tipo
+CREATE TABLE tipos
 (
 	codigo int unsigned not null auto_increment,
     tipo varchar(40) not null,
     PRIMARY KEY (codigo)
 );
 
-CREATE TABLE instrutor
+CREATE TABLE instrutores
 (
 	codigo int unsigned not null auto_increment,
     instrutor varchar(45) not null,
@@ -17,7 +17,17 @@ CREATE TABLE instrutor
     PRIMARY KEY (codigo)
 );
 
-CREATE TABLE curso
+CREATE TABLE cursos
 (
-
+	codigo int unsigned not null auto_increment,
+    curso varchar(45) not null,
+    tipo_codigo int unsigned default null,
+    instrutor_codigo int unsigned default null,
+    valor double not null,
+    PRIMARY KEY (codigo),
+    CONSTRAINT fk_cursos_tipos FOREIGN KEY (tipo_codigo) REFERENCES tipos (codigo),
+    CONSTRAINT fk_cursos_instrutores FOREIGN KEY (instrutor_codigo) REFERENCES instrutores (codigo)
 );
+
+RENAME TABLE tipo TO tipos;
+RENAME TABLE instrutor TO instrutores;
